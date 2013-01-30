33904c4fba0f6db62b888e8d50064bed38a1406f
Austin Shenk <ashenkster69@gmail.com>
2013-01-29 22:51:43 -0500

    Fixed new list errors and started redoing waypoint system

--- Assets/Resources/GameObjects/Scripts/MilitaryUnit.js
+++ Assets/Resources/GameObjects/Scripts/MilitaryUnit.js
@@ -3,7 +3,7 @@
 public class MilitaryUnit extends MovableAttackableDamagable implements Unit{
 
 	function clearWaypoints(){ //Clear Commands and Waypoints along with Waypoint objects
-		for(var i=0;i<waypoints.length;i++){ //Destroy all waypoint objects
+		for(var i=0;i<waypoints.Count;i++){ //Destroy all waypoint objects
 			Destroy(waypoints[i]);
 		}
 		commands.Clear();
@@ -11,30 +11,32 @@ public class MilitaryUnit extends MovableAttackableDamagable implements Unit{
 	}
 
 	function addCommand(hit:RaycastHit){ //Add Command and create new Waypoint
-		commands.Add(hit);
-		addWaypoint();
+		commands.Add(new Command(hit, Decide(hit)));
+		addWaypoint(hit);
 	}
 	
 	function setCommand(hit:RaycastHit){ //Set first Command to new hit
-		if(commands.length > 0){ //Clear Commands
+		if(commands.Count > 0){ //Clear Commands
 			clearWaypoints();
 		}
-		commands.Add(hit);
-		DecideandAct(hit);
+		commands.Add(new Command(hit, DecideandAct(hit)));
 	}
 	
-	function addWaypoint(){
-		var index:int = commands.length-1;
-		var hit:RaycastHit = commands[index];
+	function addWaypoint(hit:RaycastHit){
 		var pos:Vector3 = hit.point;
-		pos.y += .25;
+		pos.y += .5;
 		var newWaypoint:Waypoint;
-		if(commands.length <= 1){
-			newWaypoint = new Waypoint(pos, pos);
+		if(commands.Count <= 1){
+			Waypoint.prev = pos;
+			Waypoint.pos = pos;
+			newWaypoint = ScriptableObject.CreateInstance.<Waypoint>();
 		}
 		else{
-			newWaypoint = new Waypoint(commands[index-1], pos);
+			Waypoint.prev = commands[commands.Count-1].hit.point;
+			Waypoint.pos = pos;
+			newWaypoint = ScriptableObject.CreateInstance.<Waypoint>();
 		}
+		waypoints.Add(newWaypoint);
 	}
 	
 	function Decide(hit:RaycastHit):String{
@@ -42,7 +44,7 @@ public class MilitaryUnit extends MovableAttackableDamagable implements Unit{
 		//Some type of Unit or Building
 		if(target.GetComponent(RTSObject) != null){
 			if(target.GetComponent(RTSObject).alliance != 0 && target.GetComponent(RTSObject).alliance != alliance){
-				return "Attack";
+				return "Attackable";
 			}
 			else{
 				return "Follow";
@@ -50,19 +52,35 @@ public class MilitaryUnit extends MovableAttackableDamagable implements Unit{
 		}
 		//Ground
 		else{
-			return "Move";
+			return "Movable";
 		}
 	}
 	
-	function DecideandAct(hit:RaycastHit){
+	function DecideandAct(hit:RaycastHit):String{
 		var decision:String = Decide(hit);
 		switch(decision){
-			case "Move":
+			case "Movable":
 			Move(hit.point, true);
 			break;
 			
-			case "Attack":
+			case "Attackable":
 			break;
 		}
+		return decision;
+	}
+	
+	function Selected(select:boolean){
+		super.Selected(select);
+		for(var i:int=0;i<waypoints.Count;i++){
+			waypoints[i].renderer.enabled = select;
+		}
+	}
+	
+	function Update(){
+		if(waypoints.Count == 0) return;
+		if(GetComponent(commands[0].type).Ended()){
+			Debug.Log("Ended");
+		}
+		waypoints[0].line.SetPosition(0, transform.position);
 	}
 }
