1b95ee7c6ccb2fc38a96eea3b8fb9a82bd2b2725
Austin Shenk <ashenkster69@gmail.com>
2013-01-29 22:43:25 -0500

    Changed to ScriptableObject instead
    Now I can create a Waypoint like a normal object. I am actually thinking
    about creating a struct instead which will make it even more
    lightweight.

--- Assets/Resources/GameObjects/Scripts/Waypoint.js
+++ Assets/Resources/GameObjects/Scripts/Waypoint.js
@@ -1,10 +1,16 @@
 #pragma strict
 
-public class Waypoint extends MonoBehaviour{
-	function Waypoint(prev:Vector3, pos:Vector3){
-		var prefab:GameObject = Resources.Load("Game_Objects/Prefabs/Waypoint");
-		var waypoint:GameObject = Instantiate(prefab, pos, Quaternion(0,0,0,0));
-		var line:LineRenderer = waypoint.AddComponent(LineRenderer);
-		line.SetPosition(0,prev);
+public class Waypoint extends ScriptableObject{
+	static var prev:Vector3;
+	static var pos:Vector3;
+	public var renderer:Renderer;
+	public var line:LineRenderer;
+	function OnEnable (){
+		var prefab:GameObject = Resources.Load("GameObjects/Prefabs/Waypoint");
+		var waypoint:GameObject = Instantiate(prefab, Waypoint.pos, Quaternion(0,0,0,0));
+		renderer = waypoint.renderer;
+		line = waypoint.AddComponent(LineRenderer);
+		line.enabled = true;
+		line.SetPosition(0,Waypoint.prev);
 	}
 }
