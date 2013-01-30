d8f9fa32582c074279e4d5e6fc4dff583af517f5
Austin Shenk <ashenkster69@gmail.com>
2013-01-29 22:44:08 -0500

    Fixed the changes needed when changing list types

--- Assets/Resources/GameObjects/Scripts/UnitManager.js
+++ Assets/Resources/GameObjects/Scripts/UnitManager.js
@@ -1,14 +1,14 @@
 #pragma strict
 
 public class UnitManager{
-	static function sendUnitInfo(units:Array, hit:RaycastHit, multiCommand:boolean){
+	static function sendUnitInfo(units:List.<RTSObject>, hit:RaycastHit, multiCommand:boolean){
 		var center:Vector3;
-		for(var i:int=0;i<units.length;i++){
+		for(var i:int=0;i<units.Count;i++){
 			var temp:RTSObject = units[i] as RTSObject;
 			center += temp.transform.position;
 		}
-		center = center/units.length;
-		for(i=0;i<units.length;i++){
+		center = center/units.Count;
+		for(i=0;i<units.Count;i++){
 			temp = units[i] as RTSObject;
 			if(temp.GetComponent(Movable) != null){
 				temp.GetComponent(Movable).setCommandPointOffset(temp.transform.position-center);
