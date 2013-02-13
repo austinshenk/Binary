b20fe9c6f75f0b539092d17ce2280f50fd8bed68
Austin Shenk <ashenkster69@gmail.com>
2013-02-12 21:48:20 -0500

    Changed all JS Arrays to now be genric lists
    Generic lists are much faster and lighter weight than JS Arrays.

--- Assets/Resources/GameObjects/Scripts/Resource.js
+++ Assets/Resources/GameObjects/Scripts/Resource.js
@@ -1,19 +1,20 @@
 #pragma strict
+import System.Collections.Generic;
 
 public class Resource extends StationaryNeutral{
-	public var currentResources = new Array();
-	public var resourcesAllowed = new Array();
-	public var gatherAmount = new Array();
+	public var currentResources:List.<int> = new List.<int>();
+	public var resourcesAllowed:List.<String> = new List.<String>();
+	public var gatherAmount:List.<int> = new List.<int>();
 	
 	function Start () {
-		for(var i=0;i<resourcesAllowed.length;i++){
-			if(currentResources[i] == null) currentResources.Push(0);
+		for(var i=0;i<resourcesAllowed.Count;i++){
+			if(currentResources[i] == null) currentResources.Add(0);
 		}
 		GameObject.FindGameObjectWithTag("GameManager").GetComponent(GameManager).addResourceStockpile(this);
 	}
 
-	function gatherResources(gathererNames:Array, gathererAmounts:Array){
-		for(var i=0;i<resourcesAllowed.length;i++){
+	function gatherResources(gathererNames:List.<String>, gathererAmounts:List.<int>){
+		for(var i=0;i<resourcesAllowed.Count;i++){
 			var amount:int = currentResources[i];
 			var deduct:int = gatherAmount[i];
 			if(amount-deduct <= 0){
@@ -25,9 +26,9 @@ public class Resource extends StationaryNeutral{
 		}
 		Resource.combineResources(gathererNames, gathererAmounts, resourcesAllowed, currentResources, gatherAmount);
 	}
-	static function combineResources(targetNames:Array, targetAmounts:Array, sourceNames:Array, sourceAmounts:Array, transferAmounts:Array){
-		for(var i:int = 0;i<sourceNames.length;i++){
-			for(var j:int=0;j<targetNames.length;j++){
+	static function combineResources(targetNames:List.<String>, targetAmounts:List.<int>, sourceNames:List.<String>, sourceAmounts:List.<int>, transferAmounts:List.<int>){
+		for(var i:int = 0;i<sourceNames.Count;i++){
+			for(var j:int=0;j<targetNames.Count;j++){
 				if(sourceNames[i].Equals(targetNames[j])){
 					var amount1:int = sourceAmounts[i];
 					var amount2:int = transferAmounts[i];
