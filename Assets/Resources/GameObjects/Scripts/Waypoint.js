97129931b5012669c20345edad52266fc22b311d
Austin Shenk <ashenkster69@gmail.com>
2013-02-12 21:47:05 -0500

    Fixed Waypoint bug and moved prefab variable to static type

--- Assets/Resources/GameObjects/Scripts/Waypoint.js
+++ Assets/Resources/GameObjects/Scripts/Waypoint.js
@@ -1,16 +1,15 @@
 #pragma strict
 
 public class Waypoint extends ScriptableObject{
+	static var prefab:GameObject = Resources.Load("GameObjects/Prefabs/Waypoint") as GameObject;
 	static var prev:Vector3;
 	static var pos:Vector3;
-	public var renderer:Renderer;
 	public var line:LineRenderer;
 	function OnEnable (){
-		var prefab:GameObject = Resources.Load("GameObjects/Prefabs/Waypoint");
 		var waypoint:GameObject = Instantiate(prefab, Waypoint.pos, Quaternion(0,0,0,0));
-		renderer = waypoint.renderer;
 		line = waypoint.AddComponent(LineRenderer);
 		line.enabled = true;
 		line.SetPosition(0,Waypoint.prev);
+		line.SetPosition(1,Waypoint.pos);
 	}
 }
