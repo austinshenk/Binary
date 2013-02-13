016ba4f504ea2a917381e35232cf5d93553c71bb
Austin Shenk <ashenkster69@gmail.com>
2013-02-12 21:49:57 -0500

    Added interruptable parameter to Move command

--- Assets/Resources/GameObjects/Scripts/MovableInterface.js
+++ Assets/Resources/GameObjects/Scripts/MovableInterface.js
@@ -1,7 +1,7 @@
 #pragma strict
 
 public interface Movable{
-	function Move(point:Vector3);
+	function Move(point:Vector3, interruptable:boolean);
 	function setCommandPointOffset(point:Vector3);
 	function setFlocking(f:boolean);
 	function Ended():boolean;
