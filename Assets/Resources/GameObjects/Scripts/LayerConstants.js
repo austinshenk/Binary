d16ff3564f49172db4e268618c27c8a61932aa81
Austin Shenk <ashenkster69@gmail.com>
2013-02-12 21:52:04 -0500

    Changed variables to be actual members of class

--- Assets/Resources/GameObjects/Scripts/LayerConstants.js
+++ Assets/Resources/GameObjects/Scripts/LayerConstants.js
@@ -1,7 +1,8 @@
 #pragma strict
-
-public static final var GROUND:int = 8;
-public static final var OBSTACLES:int = 9;
-public static final var OBJECTS:int = 10;
-public static final var MINIMAP:int = 11;
-public static final var FOG:int = 12;
+public static class LayerConstants{
+	public final var GROUND:int = 8;
+	public final var OBSTACLES:int = 9;
+	public final var OBJECTS:int = 10;
+	public final var MINIMAP:int = 11;
+	public final var FOG:int = 12;
+}
