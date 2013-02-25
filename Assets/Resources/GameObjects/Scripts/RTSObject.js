2eeb523f04112ff8646a5466c80a60ecb6352354
Austin Shenk <ashenkster69@gmail.com>
2013-02-25 00:00:24 -0500

    Added waypoint system to RTSObjects
    Added beggining of resource system for buildings. Going to change this
    but for now is compatible.

--- Assets/Resources/GameObjects/Scripts/RTSObject.js
+++ Assets/Resources/GameObjects/Scripts/RTSObject.js
@@ -10,9 +10,11 @@ public class RTSObject extends MonoBehaviour implements Commandable{
 	public var height:float = 0; 
 	public var actions:TextAsset[] = new TextAsset[9];
 	public var hiddenActions:TextAsset[] = new TextAsset[0];
+	public var resourceNames:String[] = new String[0];
+	public var resourceAmounts:int[] = new int[0];
 	public var sightRange:float = 0;
 	public var priority:float = 0;
-	public var waypoints:List.<Waypoint> = new List.<Waypoint>();
+	public var waypoint:Waypoint;
 	public var commands:List.<Command> = new List.<Command>();
 	private var selectionPlaneRenderer:Renderer;
 	
@@ -21,6 +23,7 @@ public class RTSObject extends MonoBehaviour implements Commandable{
 		renderer.material.color = Selectable.colorID[team];
 		Minimap.addToMinimap(this.gameObject);
 		selectionPlaneRenderer = transform.Find("SelectedPlane").GetComponent(Renderer);
+		waypoint = ScriptableObject.CreateInstance.<Waypoint>();
 		
 		for(var i=0;i<actions.length;i++){ //Add if Action exists
 			if(actions[i] != null){ //Add Action to this
@@ -41,17 +44,114 @@ public class RTSObject extends MonoBehaviour implements Commandable{
 	
 	function Selected(select:boolean){
 		peekSelected(select);
+		if(waypoint.line != null)
+			waypoint.line.enabled = select;
 	}
 	
 	function isSelected():boolean{ //Return state of renderer for selection plane
 		return selectionPlaneRenderer.enabled;
 	}
 	
-	function addCommand(hit:RaycastHit){}
-	
-	function setCommand(hit:RaycastHit){}
-	
 	function Kill(){ //Destroy this
 		Destroy(this.gameObject);
+	}
+	function clearCommands(){ //Clear Commands and Waypoints along with Waypoint objects
+		commands.Clear();
+		waypoint.destroy();
+	}
+
+	function addCommand(hit:RaycastHit, multiCommand:boolean){ //Add Command and create new Waypoint
+		if(!multiCommand){
+			setCommand(hit);
+			return;
+		}
+		var c:Command = ScriptableObject.CreateInstance.<Command>();
+		c.hit = hit;
+		if(commands.Count == 0)
+			c.type = DecideandAct(hit);
+		else
+			c.type = Decide(hit);
+		commands.Add(c);
+		hit.point.y += .5;
+		if(commands.Count <= 1){
+			waypoint.create(hit.point, hit.point);
+		}
+		else{
+			waypoint.create(commands[commands.Count-2].hit.point, hit.point);
+		}
+	}
+	
+	function setCommand(hit:RaycastHit){ //Set first Command to new hit
+		if(commands.Count > 0){ //Clear Commands
+			clearCommands();
+		}
+		var c:Command = ScriptableObject.CreateInstance.<Command>();
+		c.hit = hit;
+		c.type = DecideandAct(hit);
+		commands.Add(c);
+	}
+	
+	function Decide(hit:RaycastHit):String{
+		var target:Collider = hit.collider;
+		//Some type of Unit or Building
+		if(target.GetComponent(RTSObject) != null){
+			if(target.GetComponent(RTSObject).alliance != 0 && target.GetComponent(RTSObject).alliance != alliance){
+				return "Attack";
+			}
+			else{
+				//Follow;
+			}
+		}
+		//Ground
+		else{
+			return "Move";
+		}
+		return "";
+	}
+	
+	function getParams(action:String, hit:RaycastHit):Object[]{
+		var params:Object[];
+		switch(action){
+			case "Move":
+			params = [hit, true];
+			break;
+			
+			case "Attack":
+			break;
+		}
+		return params;
+	}
+	
+	function DecideandAct(hit:RaycastHit):String{
+		var decision:String = Decide(hit);
+		var action:Action = GetComponent(decision) as Action;
+		if(action != null){
+			var params:Object[] = getParams(decision, hit);
+			action.Begin(params);
+		}
+		return decision;
+	}
+	
+	function Act(){
+		var params:Object[] = getParams(commands[0].type, commands[0].hit);
+		var action:Action = GetComponent(commands[0].type) as Action;
+		action.Begin(params);
+	}
+	
+	function Update(){
+		if(commands.Count == 0) return;
+		var a:Action = GetComponent(commands[0].type) as Action;
+		if(a == null) return;
+		if(a.Ended()){
+			if(waypoint.vertexCount-1 == commands.Count){
+				waypoint.pop();
+			}
+			commands.RemoveAt(0);
+			if(commands.Count != 0)
+				Act();
+			return;
+		}
+		if(waypoint.line != null)
+			waypoint.line.SetPosition(0, transform.position);
 	}
 }
